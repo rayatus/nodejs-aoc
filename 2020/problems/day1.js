96143fefaa500aec1dcc798997bdb7e587f53382
Jorge Sancho <12736990+rayatus@users.noreply.github.com>
2021-08-13 14:13:18 +0200

    solving Day 1 Part 1

--- 2020/problems/day1.js
+++ 2020/problems/day1.js
@@ -1,7 +1,17 @@
 
 const solve_part_1 = (inputData)=>{
-  return "this is day 1, part 1"
+  let data = inputData.split('\r\n')
+  
+  for (let i = 0; i < data.length ; i++){
+    let element1 = Number(data[i])
 
+    for (let j = i+1; j < data.length ; j++){
+      let element2 = Number(data[j])
+
+      if ( ( element1 + element2 ) == 2020 )
+        return element1 * element2
+    }
+  }
 }
 
 const solve_part_2 = (inputData)=>{
