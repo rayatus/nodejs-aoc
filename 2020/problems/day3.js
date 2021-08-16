aec6a70d6cb1cf245f12adeb18da48e51b0b12a0
Jorge Sancho <12736990+rayatus@users.noreply.github.com>
2021-08-16 18:46:13 +0200

    solve day 3 part 1

--- 2020/problems/day3.js
+++ 2020/problems/day3.js
@@ -1,7 +1,30 @@
+function adjustInputData (inputData){
+  return inputData.split('\r\n')
+}
 
 const solve_part_1 = (inputData)=>{
-  let result = "this is day 3, part 1"
-  return result
+  let position = { x : 0, y : 1 }
+  const movement = { down : 1, right : 3 }
+  const tree = '#'
+  let trees = 0
+
+  let data = adjustInputData(inputData)
+
+  for (position.y; position.y < data.length; position.y += movement.down) {
+    position.x += movement.right
+    let row = data[position.y].split('')
+
+    let i = 0
+    while ( row.length < position.x + 1 ) {
+      row.push(row[i])
+      i++
+    } 
+
+    const element = row[position.x]
+    if (element == tree)
+     trees += 1
+  }
+  return trees 
 }
 
 const solve_part_2 = (inputData)=>{
