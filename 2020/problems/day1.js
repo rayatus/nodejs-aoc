b88e1fcb61349aac1ae4383e4a79ca4e4412024a
Jorge Sancho <12736990+rayatus@users.noreply.github.com>
2021-08-16 10:04:38 +0200

    solve Day1 both parts

--- 2020/problems/day1.js
+++ 2020/problems/day1.js
@@ -1,6 +1,13 @@
 
+function adjustInputData (inputData){
+  return inputData.split('\r\n')
+}
+
+/*
+ Let's find 2 numbers that sum 2020 and return their multiplication 
+*/
 const solve_part_1 = (inputData)=>{
-  let data = inputData.split('\r\n')
+  let data = adjustInputData(inputData)
   
   for (let i = 0; i < data.length ; i++){
     let element1 = Number(data[i])
@@ -14,8 +21,29 @@ const solve_part_1 = (inputData)=>{
   }
 }
 
+/*
+ Let's find 3 numbers that sum 2020 and return their multiplication
+*/  
 const solve_part_2 = (inputData)=>{
-  return "this is day 1, part 2"
+  
+  let data = adjustInputData(inputData)
+
+  for (let i = 0; i < data.length ; i++){
+    let element1 = Number(data[i])
+
+    for (let j = i+1; j < data.length ; j++){
+      let element2 = Number(data[j])
+
+      for (let k = j+1; k < data.length ; k++){
+        let element3 = Number(data[k])
+
+        if ( ( element1 + element2 + element3 ) == 2020 )
+        return element1 * element2 * element3
+
+      }       
+    }
+  } 
+
 }
 
 export { solve_part_1, solve_part_2 }
