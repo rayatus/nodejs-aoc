0cc7089931097540d831db41537960d8f2fa4b0d
Jorge Sancho <12736990+rayatus@users.noreply.github.com>
2021-08-16 19:01:51 +0200

    solve Day 3 part 2 and refactor of part 1

--- 2020/problems/day3.js
+++ 2020/problems/day3.js
@@ -2,34 +2,39 @@ function adjustInputData (inputData){
   return inputData.split('\r\n')
 }
 
-const solve_part_1 = (inputData)=>{
-  let position = { x : 0, y : 1 }
-  const movement = { down : 1, right : 3 }
+function explorePath( movement, inputData){
+  let position = { x : 0, y : 0 }
   const tree = '#'
   let trees = 0
 
   let data = adjustInputData(inputData)
 
-  for (position.y; position.y < data.length; position.y += movement.down) {
+  for (position.y = movement.down ; position.y < data.length; position.y += movement.down) {
     position.x += movement.right
-    let row = data[position.y].split('')
+    const row = data[position.y].split('')
 
-    let i = 0
-    while ( row.length < position.x + 1 ) {
-      row.push(row[i])
-      i++
-    } 
+    if ( row.length <= position.x )
+      position.x = position.x - row.length
 
     const element = row[position.x]
     if (element == tree)
-     trees += 1
+      trees += 1   
+
   }
   return trees 
 }
 
+const solve_part_1 = (inputData)=>{
+  return explorePath({ down : 1, right : 3 }, inputData)
+}
+
 const solve_part_2 = (inputData)=>{
-  let result = "this is day 3, part 2"
-  return result
+  return explorePath({ down : 1, right : 1 }, inputData) * 
+         explorePath({ down : 1, right : 3 }, inputData) *
+         explorePath({ down : 1, right : 5 }, inputData) *
+         explorePath({ down : 1, right : 7 }, inputData) *
+         explorePath({ down : 2, right : 1 }, inputData)
+  
 }
 
 export { solve_part_1, solve_part_2 }
