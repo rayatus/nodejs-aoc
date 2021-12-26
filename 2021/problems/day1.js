505af8110c722e8c97b4c017cf00c0d901fa0c22
Jorge Sancho <12736990+rayatus@users.noreply.github.com>
2021-12-26 23:21:21 +0100

    day1, problem 2 solved (pending to improve)

--- 2021/problems/day1.js
+++ 2021/problems/day1.js
@@ -1,17 +1,15 @@
 /*
  Count the number of times a depth measurement increases
 */
-const solve_part_1 = (inputData, inputUtils)=>{
-  
-
+const solve_part_1 = (inputData, inputUtils) => {
   let prevElement
   let currentElement
   let result = 0
 
   const data = inputUtils.inputDataToInt(inputData)
-  for (let i = 0; i < data.length ; i++){
+  for (let i = 0; i < data.length; i++) {
     currentElement = data[i]
-    if (prevElement !== undefined ){
+    if (prevElement !== undefined) {
       if (currentElement > prevElement) result += 1
     }
     prevElement = currentElement
@@ -20,12 +18,71 @@ const solve_part_1 = (inputData, inputUtils)=>{
 }
 
 /*
- 
-*/  
-const solve_part_2 = (inputData)=>{
-  
-  
+ Count the number of times the sum of measurements in this sliding window increases from the previous sum
+*/
 
+function adjustInputDataPart2(inputData) {
+  let Windows = []
+  let Window1 = []
+  let Window2 = []
+  let Window3 = []
+  let array1 = 0
+  let array2 = -1
+  let array3 = -2
+
+  inputData.forEach((measure) => {
+    if (array1 == 0) {
+      Window1 = new Array()
+      Windows.push(Window1)
+    }
+
+    if (array2 == 0) {
+      Window2 = new Array()
+      Windows.push(Window2)
+    }
+
+    if (array3 == 0) {
+      Window3 = new Array()
+      Windows.push(Window3)
+    }
+
+    array1++
+    array2++
+    array3++
+
+    if (array1 > 0) Window1.push(measure)
+    if (array2 > 0) Window2.push(measure)
+    if (array3 > 0) Window3.push(measure)
+
+    if (array1 == 3) {
+      array1 = 0
+    }
+    if (array2 == 3) {
+      array2 = 0
+    }
+    if (array3 == 3) {
+      array3 = 0
+    }
+  })
+
+  return Windows
+}
+
+const solve_part_2 = (inputData, inputUtils) => {
+  let prevSum
+  let result = 0
+
+  const data = adjustInputDataPart2(inputUtils.inputDataToInt(inputData))
+  data.forEach((window) => {
+    let currentSum = window.reduce((total, x) => total + x)
+
+    if (prevSum !== undefined) {
+      if (currentSum > prevSum) result++
+    }
+    prevSum = currentSum
+  })
+
+  return result
 }
 
 export { solve_part_1, solve_part_2 }
--- aoc.js
+++ aoc.js
@@ -4,140 +4,140 @@
 
 --------------------------------------------------------------------------------*/
 
-import fs from 'fs'
-import minimist from 'minimist'
+import fs from "fs"
+import minimist from "minimist"
 
 const _YEAR_NOT_DEFINED = -1
-const _DAY_NOT_DEFINED  = -1
+const _DAY_NOT_DEFINED = -1
 const _PART_NOT_DEFINED = -1
 
 const args = minimist(process.argv.slice(2))
 let yearToRun = args["year"] || _YEAR_NOT_DEFINED
-let dayToRun  = args["day"]  || _DAY_NOT_DEFINED
-let part      = args["part"] || _PART_NOT_DEFINED
-let help      = args["help"]
+let dayToRun = args["day"] || _DAY_NOT_DEFINED
+let part = args["part"] || _PART_NOT_DEFINED
+let help = args["help"]
 
-if (help !== undefined){
+if (help !== undefined) {
   showDocumentation()
-}else{
+} else {
   //Check if arguments are correct
-  if ( ( yearToRun != _YEAR_NOT_DEFINED  && isValidYear(yearToRun) == false ) 
-    || ( dayToRun  != _DAY_NOT_DEFINED   && isValidDay(dayToRun)   == false ) 
-    || ( part      != _PART_NOT_DEFINED  && isValidPart(part)      == false ) ) {
-    showDocumentation() 
+  if (
+    (yearToRun != _YEAR_NOT_DEFINED && isValidYear(yearToRun) == false) ||
+    (dayToRun != _DAY_NOT_DEFINED && isValidDay(dayToRun) == false) ||
+    (part != _PART_NOT_DEFINED && isValidPart(part) == false)
+  ) {
+    showDocumentation()
   } else {
-    await executeAoC(yearToRun, dayToRun, part)
+    executeAoC(yearToRun, dayToRun, part)
   }
 }
 
-
-//Executes corresponding exercices
+//Executes corresponding exercises
 async function executeAoC(yearToRun, dayToRun, part) {
-  const dir = await fs.promises.opendir('./');
-  for await (const dirent of dir) {
-    if ( isValidYear(dirent.name) == true && (dirent.name == yearToRun || yearToRun == _YEAR_NOT_DEFINED ) ){
-      console.log(`Executing problems from AoC ${dirent.name}`)
-
-      let yearFolder     = `./${dirent.name}`
-      let problemFolder  = `${yearFolder}/problems`
-      let inputFolder    = `${yearFolder}/input`
-
-      fs.readdir(problemFolder, (err, files) => {
-        if (err){
-          console.error(`Error while reading contents of path:${path}`)
-          console.error(err)
-        }else{
-          files.forEach( problemFile => {
-
-            let day           = problemFile.slice(3,-3)  // day1.js ==> 1
-            let inputFilename = problemFile.slice(0,-3)  // day1.js ==> day1
-            let inputPath     = `${inputFolder}/${inputFilename}`
-
-            if ( isValidDay(day) && ( day == dayToRun || dayToRun == _DAY_NOT_DEFINED ) )
-              try {
-                const inputData = fs.readFileSync(inputPath, 'utf8')  
-                run( `${problemFolder}/${problemFile}`, part, inputData )
-              } catch (error) {
-                console.error(`Error while reading contents of input file:${inputPath}`)
-                console.error(err)
-              }
-          })
-        }
-      })
-    }
-  }
-}
-
-//Imports the solutions for the given year/Day
-async function getAoCModule(modulePath){
-    
-  try{
-     const aocModule = await import(modulePath)
-     return aocModule
-  } catch (error) {
-    console.error(`Something wrong happend while importing package '${modulePath}' : ${error}`)
-  } 
-}
-
-//Executes corresponding part, or both, for a given day of the given year
-async function run(modulePath, part, input){
- 
-  try{
+  try {
     const inputUtil = await import("./lib/inputUtils.js")
 
-    try{
-      const aocModule = await getAoCModule(modulePath)
-      
-      
-      console.log(`Solving ${modulePath}`)
-      if (part == 1 || part == _PART_NOT_DEFINED){
-        const result1 = aocModule.solve_part_1(input, inputUtil)
-        console.log(`Solution for part 1 => ${result1}`)
-      }
-      if (part == 2 || part == _PART_NOT_DEFINED){
-        const result2 = aocModule.solve_part_2(input, inputUtil)
-        console.log(`Solution for part 2 => ${result2}`)
-      }
-          
-    } catch (error) {
-      console.error(`Something wrong happend while executing solutions for '${modulePath}', part='${part}' : ${error}`)
-    } 
+    const dir = fs.readdirSync("./")
+    for (const dirContent of dir) {
+      if (isValidYear(dirContent) == true && (dirContent == yearToRun || yearToRun == _YEAR_NOT_DEFINED)) {
+        console.log(`Executing AoC ${dirContent}`)
 
-  } catch(error){
+        let yearFolder = `./${dirContent}`
+        let problemFolder = `${yearFolder}/problems`
+        let inputFolder = `${yearFolder}/input`
+
+        const files = fs.readdirSync(problemFolder)
+        files.forEach((file) => {
+          let day = file.slice(3, -3) // day1.js ==> 1
+          let inputFilename = file.slice(0, -3) // day1.js ==> day1
+          let inputPath = `${inputFolder}/${inputFilename}`
+
+          if (isValidDay(day) && (day == dayToRun || dayToRun == _DAY_NOT_DEFINED))
+            try {
+              const inputData = fs.readFileSync(inputPath, "utf8")
+
+              run(`${problemFolder}/${file}`, part, inputData, inputUtil)
+            } catch (error) {
+              console.error(`Error while reading contents of input file:${inputPath}`)
+              console.error(error)
+            }
+        })
+      }
+    }
+  } catch (error) {
     console.error(`Something wrong happend while importing inputUtils.js : ${error}`)
   }
 }
 
+//Imports the solutions for the given year/Day
+async function getAoCModule(modulePath) {
+  try {
+    const aocModule = await import(modulePath)
+    return aocModule
+  } catch (error) {
+    console.error(`Something wrong happend while importing package '${modulePath}' : ${error}`)
+  }
+}
+
+//Executes corresponding part, or both, for a given day of the given year
+async function run(modulePath, part, input, inputUtil) {
+  let runPart = 0
+
+  try {
+    const aocModule = await getAoCModule(modulePath)
+
+    console.log(`Solving ${modulePath}`)
+    if (part == 1 || part == _PART_NOT_DEFINED) {
+      runPart = 1
+      const result1 = aocModule.solve_part_1(input, inputUtil)
+      console.log(`Solution for part 1 => ${result1}`)
+    }
+    if (part == 2 || part == _PART_NOT_DEFINED) {
+      runPart = 2
+      const result2 = aocModule.solve_part_2(input, inputUtil)
+      console.log(`Solution for part 2 => ${result2}`)
+    }
+  } catch (error) {
+    console.error(`Something wrong happend while executing solutions for '${modulePath}', part='${runPart}' : ${error}`)
+  }
+}
+
 //Verifies if a given Year has a correct value
-function isValidYear(year){
+function isValidYear(year) {
   let isValid = false
-  if ( isNaN(year) == true ) return isValidPart
-  if ( ( year >= 2020 && year <= 9999 ) ) isValid = true
+  if (isNaN(year) == true) return isValidPart
+  if (year >= 2020 && year <= 9999) isValid = true
   return isValid
 }
 
 //Verifies if a given Day has a correct value
-function isValidDay(day){
+function isValidDay(day) {
   let isValid = false
-  if ( isNaN(day) == true ) return isValidPart
-  if ( ( day > 0 && day <= 31 ) ) isValid = true
+  if (isNaN(day) == true) return isValidPart
+  if (day > 0 && day <= 31) isValid = true
   return isValid
 }
 
 //Verifies if a given Part has a correct value
-function isValidPart(part){
+function isValidPart(part) {
   let isValid = false
-  if ( isNaN(part) == true ) return isValidPart
-  if ( ( part > 0 && part < 3 ) )  isValid = true
+  if (isNaN(part) == true) return isValidPart
+  if (part > 0 && part < 3) isValid = true
   return isValid
 }
 
 //Returns appDocumentation
-function showDocumentation(){
+function showDocumentation() {
   console.log(" Please execute it as: aoc.js --year --day --part")
   console.log(" All following arguments are optional:")
-  console.log(" --year: specifies which year to execute. If no year is provided all of them will be executed. (i.e --year 2020) ")
-  console.log(" --day: specifies exactly which problem to execute. If no day is provided all of them will be executed (i.e. --day 1) ")
-  console.log(" --part: specifies exactly which problem's part to execute, Advents of Code consists of 2 parts per each day so allowed values are 1 and 2 (i.e. --part 2). If no part is provided all of them (1 and 2) are going to be executed")
+  console.log(
+    " --year: specifies which year to execute. If no year is provided all of them will be executed. (i.e --year 2020) "
+  )
+  console.log(
+    " --day: specifies exactly which problem to execute. If no day is provided all of them will be executed (i.e. --day 1) "
+  )
+  console.log(
+    " --part: specifies exactly which problem's part to execute, Advents of Code consists of 2 parts per each day so allowed values are 1 and 2 (i.e. --part 2). If no part is provided all of them (1 and 2) are going to be executed"
+  )
   console.log("\nFor instance aoc.js --year 2021 --day 1 --part 1")
 }
