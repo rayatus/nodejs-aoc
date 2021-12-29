6ea69ddf66cb71e8081c6c0437548fc2109d3d24
Jorge Sancho <12736990+rayatus@users.noreply.github.com>
2021-12-29 10:29:44 +0100

    Day 2 - Part 2

--- 2021/problems/day2.js
+++ 2021/problems/day2.js
@@ -28,8 +28,36 @@ const solve_part_1 = (inputData, inputUtils) => {
 }
 
 const solve_part_2 = (inputData, inputUtils) => {
-  let result
-  return result
+  let depth = 0
+  let horizontal = 0
+  let aim = 0
+
+  const _AIM = "aim"
+  const _FORWARD = "forward"
+  const _UP = "up"
+  const _DOWN = "down"
+
+  const data = inputUtils.inputDataToLines(inputData)
+
+  data.forEach((element) => {
+    const movement = element.split(" ")[0]
+    const magnitude = parseInt(element.split(" ")[1])
+
+    switch (movement) {
+      case _FORWARD:
+        horizontal += magnitude
+        depth += aim * magnitude
+        break
+      case _UP:
+        aim -= magnitude
+        break
+      case _DOWN:
+        aim += magnitude
+        break
+    }
+  })
+
+  return depth * horizontal
 }
 
 export { solve_part_1, solve_part_2 }
--- 2021/tests/day2_test.js
+++ 2021/tests/day2_test.js
@@ -3,13 +3,15 @@ import chai from "chai"
 const day = await import("../problems/day2.js")
 const inputUtils = await import("../../lib/inputUtils.js")
 const expect = chai.expect
-const inputData = "forward 5\r\ndown 5\r\nforward 8\r\nup 3\r\ndown 8\r\nforward 2\r\n"
 
 describe("Test AoC 2021 Day 2", () => {
   it("Part 1", () => {
-    expect(day.solve_part_1(inputData, inputUtils)).equal(150)
+    const inputData = "forward 5\r\ndown 5\r\nforward 8\r\nup 3\r\ndown 8\r\nforward 2\r\n"
+    const result = day.solve_part_1(inputData, inputUtils)
+    expect(result).equal(150)
   })
   it("Part 2", () => {
-    expect(day.solve_part_2(inputData, inputUtils)).equal(5)
+    const inputData = "forward 5\r\ndown 5\r\nforward 8\r\nup 3\r\ndown 8\r\nforward 2"
+    expect(day.solve_part_2(inputData, inputUtils)).equal(900)
   })
 })
