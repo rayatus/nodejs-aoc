2bbadae33aa3e3125a7231463b1891e0b548982d
Jorge Sancho <12736990+rayatus@users.noreply.github.com>
2022-12-14 23:22:27 +0100

    some minor refactoring

--- 2022/problems/day4.js
+++ 2022/problems/day4.js
@@ -1,48 +1,55 @@
 
+const GROUP_SEPARATOR = ','
+const FROM_TO_SEPARATOR = '-'
+
+function CreateGroupObject(groups, index) {
+  const group = groups[index - 1]
+
+  const Group = {
+    "from": parseInt((group.split(FROM_TO_SEPARATOR)).at(0)),
+    "to": parseInt((group.split(FROM_TO_SEPARATOR)).at(1)),
+  }
+  return Group
+}
 
 /*
-  1. each row is separated by coma ',' 2 groups
-  2. each group withing row defines 'from - to'  (by numbers (2-4)
+  1. each row is separated by coma ',' resulting in 2 groups
+  2. each group withing row defines 'from - to'  (by numbers (2-4))
   3. find in which rows one of the groups FULLY contains the other (i.e. 3-8 contains 5-6)
 */
 const solve_part_1 = (inputData, inputUtils) => {
 
-  let result = null
-  const GROUP_SEPARATOR = ','
-  const FROM_TO_SEPARATOR = '-'
   const data = inputUtils.inputDataToLines(inputData)
 
-  result = data.filter(row => {
+  return data.filter(row => {
     const groups = row.split(GROUP_SEPARATOR)
 
-    function CreateGroupObject(groups, index) {
-      const group = groups[index - 1]
 
-      const Group = {
-        "from": parseInt((group.split(FROM_TO_SEPARATOR)).at(0)),
-        "to": parseInt((group.split(FROM_TO_SEPARATOR)).at(1)),
-      }
-      return Group
-    }
 
     const group1 = CreateGroupObject(groups, 1)
     const group2 = CreateGroupObject(groups, 2)
 
     //Does Group1 containe Group2 or the other way arround?
-    const result = (group1.from <= group2.from && group1.to >= group2.to) || (group2.from <= group1.from && group2.to >= group1.to)
-    return result
-  })
+    return (group1.from <= group2.from && group1.to >= group2.to) || (group2.from <= group1.from && group2.to >= group1.to)
 
-  return result.length
+  }).length
 }
 
 /*
- 
+  1. each row is separated by coma ',' restulting in 2 groups
+  2. each group withing row defines 'from - to'  (by numbers (2-4))
+  3. find in which rows one of the groups OVERLAPS contains the other (i.e. 5-7 overlaps with 7-9)
 */
 const solve_part_2 = (inputData, inputUtils) => {
 
-  let result = null
-  return result
+
+  const GROUP_SEPARATOR = ','
+  const FROM_TO_SEPARATOR = '-'
+  const data = inputUtils.inputDataToLines(inputData)
+
+  return data.filter(x => {
+
+  }).length
 
 }
 
--- 2022/tests/day4_test.js
+++ 2022/tests/day4_test.js
@@ -12,7 +12,6 @@ const inputData = new testUtil.TestData()
   .addValueToTest("2-8,3-7")
   .addValueToTest("6-6,4-6")
   .addValueToTest("2-6,4-8")
-  .addValueToTest("5-17,1-6")
   .get()
 
 describe("Test AoC 2022 Day 4", () => {
@@ -20,6 +19,6 @@ describe("Test AoC 2022 Day 4", () => {
     expect(day.solve_part_1(inputData, inputUtils)).equal(2)
   })
   it("Part 2", () => {
-    expect(day.solve_part_2(inputData, inputUtils)).equal(70)
+    expect(day.solve_part_2(inputData, inputUtils)).equal(4)
   })
 })
