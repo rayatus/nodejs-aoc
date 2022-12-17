0219fa405fedc64c3eb919cffabc3fed839cc3c4
Jorge Sancho <joers@vestas.com>
2022-12-17 14:56:55 +0100

    load dynamially problem in test

--- 2022/tests/day4_test.js
+++ 2022/tests/day4_test.js
@@ -1,11 +1,16 @@
-import chai from "chai"
+import { expect } from "chai"
+import { fileURLToPath } from 'url';
 
-const day = await import("../problems/day4.js")
+const __filename = fileURLToPath(import.meta.url);
 const inputUtils = await import("../../lib/inputUtils.js")
-const expect = chai.expect
 const testUtil = await import("../../lib/testInputUtils.js")
+const TestUtil = new testUtil.TestData()
 
-const inputData = new testUtil.TestData()
+const metadata = TestUtil.getTestMetadata(__filename)
+
+const day = await import(metadata.pathToProblem)
+
+const inputData = TestUtil
   .addValueToTest("2-4,6-8")
   .addValueToTest("2-3,4-5")
   .addValueToTest("5-7,7-9")
@@ -14,7 +19,7 @@ const inputData = new testUtil.TestData()
   .addValueToTest("2-6,4-8")
   .get()
 
-describe("Test AoC 2022 Day 4", () => {
+describe(`Test AoC ${metadata.year} Day ${metadata.day}`, () => {
   it("Part 1", () => {
     expect(day.solve_part_1(inputData, inputUtils)).equal(2)
   })
