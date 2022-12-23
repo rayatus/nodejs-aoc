a3c41f42e0b6f939a1cdf8d221d2dadfab5cd773
Jorge Sancho <joers@vestas.com>
2022-12-23 10:13:06 +0100

    initialize test 5
    test Data will come from a TXT file instead of directlly entered on code

---
+++ 2022/problems/day5.js
@@ -0,0 +1,17 @@
+
+/*
+  
+*/
+const solve_part_1 = (inputData, inputUtils) => {
+
+  return null
+}
+
+/*
+  
+*/
+const solve_part_2 = (inputData, inputUtils) => {
+  return null
+}
+
+export { solve_part_1, solve_part_2 }
---
+++ 2022/tests/day5_test.js
@@ -0,0 +1,23 @@
+import { expect } from "chai"
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const inputUtils = await import("../../lib/inputUtils.js")
+const testUtil = await import("../../lib/testInputUtils.js")
+
+const TestData = new testUtil.TestData()
+let __testFileName = __filename.replace("day5_test.js", "day5_test_data.txt")
+const inputData = TestData.addFromFile(__testFileName).get()
+
+const metadata = TestData.getTestMetadata(__filename)
+const day = await import(metadata.pathToProblem)
+
+describe(`Test AoC ${metadata.year} Day ${metadata.day}`, () => {
+  it("Part 1", () => {
+    expect(day.solve_part_1(inputData, inputUtils)).equal("CMZ")
+  })
+  it("Part 2", () => {
+    expect(day.solve_part_2(inputData, inputUtils)).equal(4)
+  })
+})
